be56583b05d572d710cb7e0a8efebfd5d5814a74
brijitha.av <brijitha.av@m.ecsfin.com>
2023-10-11 22:24:17 +0530

    feat(landing-page): create landing page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-link">Sleep with pees</div>
+        <div className="App-link">Sleep Tech</div>
+        <div className="sub-title">PeeS</div>
+        <div className="title-theme">PeeS be with you. Sleep with PeeS!</div>
       </header>
     </div>
   );
