aa525fa29070a10a7da72a375117e60fda7ef89e
brijitha.av <brijitha.av@m.ecsfin.com>
2023-11-12 20:27:44 +0530

    feat(route): page export and routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "../../pages/landing-page/LandingPage";
-import AuthFrame from "../../pages/auth-service/AuthFrame";
 import AppLayout from "../generic/AppLayout";
 import {
   MyDiet,
@@ -11,7 +11,14 @@ import {
   TaskManagement,
   Techniques,
   PsychoEducation,
+  ScribbleSpace,
+  Analytics,
+  MyReports,
+  MyProfile,
 } from "../../pages";
+import { SleepDiaryForm } from "../../pages/dashboard/ui";
+import DoctorsDetail from "../../pages/my-doctor/ui/DoctorsDetail";
+import AuthFrame from "../../auth-service/AuthFrame";
 
 export const sleepWithPees = (
   <>
@@ -29,7 +36,13 @@ export const sleepWithPees = (
         <Route path="/playlist" element={<Playlist />} />
         <Route path="/my-doctor" element={<MyDoctor />} />
         <Route path="/psycho-education" element={<PsychoEducation />} />
-        <Route path="*" element="/dashboard" />
+        <Route path="/sleep-diary" element={<SleepDiaryForm />} />
+        <Route path="/my-scribble-space" element={<ScribbleSpace />} />
+        <Route path="/doctors-details/:id" element={<DoctorsDetail />} />
+        <Route path="/analytics-and-suggestions" element={<Analytics />} />
+        <Route path="/my-reports" element={<MyReports />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        {/* <Route path="*" element={<Dashboard />} /> */}
       </Routes>
     </AppLayout>
   </>
